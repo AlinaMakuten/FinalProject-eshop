ccee935b4fb362b1a5e8babc3beab1a2bc19321d
Alina0726 <alina.makuten@outlook.com>
2022-12-05 14:04:43 +0200

    sutvarkyti mokejimai

--- orders/views.py
+++ orders/views.py
@@ -5,55 +5,68 @@ from .models import Order, Payment, OrderProduct
 import datetime
 import json
 from products.models import Product
-from django.http import HttpResponse
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 def payments(request):
     body = json.loads(request.body)
-    print(body)
-    # order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
-    #
-    # payment = Payment(
-    #     user=request.user,
-    #     payment_id=body['transID'],
-    #     payment_method=body['payment_method'],
-    #     amount_paid=order.order_total,
-    #     status=body['status'],
-    # )
-    # payment.save()
-    #
-    # order.payment = payment
-    # order.is_ordered = True
-    # order.save()
-    #
-    # cart_items = CartItem.objects.filter(user=request.user)
-    #
-    # for item in cart_items:
-    #     orderproduct = OrderProduct()
-    #     orderproduct.order_id = order.id
-    #     orderproduct.payment = payment
-    #     orderproduct.user_id = request.user.id
-    #     orderproduct.product_id = item.product_id
-    #     orderproduct.quantity = item.quantity
-    #     orderproduct.product_price = item.product.price
-    #     orderproduct.ordered = True
-    #     orderproduct.save()
-    #
-    #     cart_item = CartItem.objects.get(id=item.id)
-    #     product_variation = cart_item.variations.all()
-    #     orderproduct = OrderProduct.objects.get(id=orderproduct.id)
-    #     orderproduct.variations.set(product_variation)
-    #     orderproduct.save()
-    #
-    #     product = Product.objects.get(id=item.product_id)  #sumažins kiekį DB
-    #     product.stock -= item.quantity
-    #     product.save()
-    #
-    #
-    # CartItem.objects.filter(user=request.user).delete()   #užsakius išvalys krepšelį
-    #
-    #
-    #
-    return render(request, 'orders/payments.html')
+    # print(body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=body['payment_method'],
+        amount_paid=order.order_total,
+        status=body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        cart_item = CartItem.objects.get(id=item.id) #manytomany negalima tiesiogiai
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
+
+        product = Product.objects.get(id=item.product_id)  #sumažins kiekį DB
+        product.stock -= item.quantity
+        product.save()
+
+
+    CartItem.objects.filter(user=request.user).delete()   #užsakius išvalys krepšelį
+
+    mail_subject = "Dėkojame už užsakymą"
+    message = render_to_string('orders/order_received_email.html', {
+        'user': request.user,
+    })
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
+
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
+
 
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
@@ -87,6 +100,7 @@ def place_order(request, total=0, quantity=0,):
             data.city = form.cleaned_data['city']
             data.postal_code = form.cleaned_data['postal_code']
             data.order_note = form.cleaned_data['order_note']
+
             data.order_total = grand_total
             data.tax = tax
             data.ip = request.META.get('REMOTE_ADDR')
@@ -102,6 +116,7 @@ def place_order(request, total=0, quantity=0,):
             data.save()
 
 
+
             order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
             context = {
                 'order': order,
@@ -114,5 +129,32 @@ def place_order(request, total=0, quantity=0,):
             return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
+
+
+def order_complete(request):
+
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context=context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
 
 
